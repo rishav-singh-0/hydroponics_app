0160d6caea07635acb7b859c7d2192e28d4c202a
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-10 22:53:35 +0530

    made card_page layout

--- lib/views/chart_page.dart
+++ lib/views/chart_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hydroponics_app/views/components/gauge.dart';
 
-import 'components/gauge_card.dart';
+import 'components/custom_card.dart';
 
 class ChartPage extends StatelessWidget {
   const ChartPage({Key? key}) : super(key: key);
@@ -10,57 +11,85 @@ class ChartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: const [
+      body: ListView(physics: const BouncingScrollPhysics(), children: [
+        const Padding(
+          padding: EdgeInsets.all(10),
+        ),
+        Text("Monitor",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 34.w, fontWeight: FontWeight.bold)),
         LinearGaugeCard(
-            label: "Temperature",
+          label: "Temperature",
+          currentValue: "60 °c",
+          cardIcon: CupertinoIcons.thermometer,
+          child: VerticalGauge(
             minRange: 0,
             maxRange: 80,
-            pointerValue: 60,
             stepSize: 20,
             stringUnit: "°c",
             colorLow: Colors.green,
             colorMid: Colors.orange,
-            colorHigh: Colors.red),
+            colorHigh: Colors.red,
+            pointerValue: 60,
+          ),
+        ),
         LinearGaugeCard(
-            label: "Humidity",
+          label: "Humidity",
+          currentValue: "90 units",
+          cardIcon: CupertinoIcons.wand_stars_inverse,
+          child: VerticalGauge(
             minRange: 0,
             maxRange: 90,
-            pointerValue: 50,
             stepSize: 20,
+            pointerValue: 50,
             stringUnit: "C",
             colorLow: Colors.lightBlue,
             colorMid: Colors.blue,
-            colorHigh: Colors.purple),
+            colorHigh: Colors.purple,
+          ),
+        ),
         LinearGaugeCard(
-            label: "Moisture",
-            minRange: 0,
-            maxRange: 4000,
-            pointerValue: 60,
-            stepSize: 800,
-            stringUnit: "",
-            colorLow: Colors.lightBlue,
-            colorMid: Colors.blue,
-            colorHigh: Colors.purple),
+          label: "Moisture",
+          currentValue: "100",
+          cardIcon: CupertinoIcons.wand_stars_inverse,
+          child: VerticalGauge(
+              minRange: 0,
+              maxRange: 4000,
+              pointerValue: 60,
+              stepSize: 800,
+              stringUnit: "",
+              colorLow: Colors.lightBlue,
+              colorMid: Colors.blue,
+              colorHigh: Colors.purple),
+        ),
         LinearGaugeCard(
-            label: "pH",
-            minRange: 0,
-            maxRange: 14,
-            pointerValue: 7.5,
-            stepSize: 1,
-            stringUnit: "",
-            colorLow: Colors.blue,
-            colorMid: Colors.orange,
-            colorHigh: Colors.red),
+          label: "pH",
+          currentValue: "100",
+          cardIcon: CupertinoIcons.lab_flask,
+          child: VerticalGauge(
+              minRange: 0,
+              maxRange: 14,
+              pointerValue: 7.5,
+              stepSize: 1,
+              stringUnit: "",
+              colorLow: Colors.blue,
+              colorMid: Colors.orange,
+              colorHigh: Colors.red),
+        ),
         LinearGaugeCard(
-            label: "Luminosity",
-            minRange: 0,
-            maxRange: 4000,
-            pointerValue: 1000,
-            stepSize: 800,
-            stringUnit: "",
-            colorLow: Colors.blue,
-            colorMid: Colors.orange,
-            colorHigh: Colors.red),
+          label: "Luminosity",
+          currentValue: "100",
+          cardIcon: CupertinoIcons.light_max,
+          child: VerticalGauge(
+              minRange: 0,
+              maxRange: 4000,
+              pointerValue: 1000,
+              stepSize: 800,
+              stringUnit: "",
+              colorLow: Colors.blue,
+              colorMid: Colors.orange,
+              colorHigh: Colors.red),
+        ),
       ]),
     );
   }
