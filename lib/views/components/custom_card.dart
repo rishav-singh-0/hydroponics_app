531c14e9e1e3167ed74481dbef3ac28692cdb0dc
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-11 03:24:01 +0530

    added more custom cards

--- lib/views/components/custom_card.dart
+++ lib/views/components/custom_card.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ElevatedCard extends StatelessWidget {
-  const ElevatedCard({
-    Key? key,
-    required this.label,
-    required this.cardIcon,
-    required this.currentValue,
-    required this.child,
-  }) : super(key: key);
+BorderRadius circularBorderRadius = BorderRadius.circular(10.0);
+var labelTextStyle = TextStyle(fontSize: 14.w, fontWeight: FontWeight.bold);
 
-  final String label;
-  final IconData cardIcon;
-  final String currentValue;
-  final Widget child;
+class ElevatedCard extends StatelessWidget {
+  const ElevatedCard({Key? key, required this.cardChild}) : super(key: key);
+
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -22,35 +16,98 @@ class ElevatedCard extends StatelessWidget {
         margin: const EdgeInsets.all(15),
         padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.white,
+          borderRadius: circularBorderRadius,
+          color: Colors.blue,
           backgroundBlendMode: BlendMode.colorBurn,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              offset: Offset(0.0, 1.0), //(x,y)
-              blurRadius: 10.0,
+          // boxShadow: const [
+          //   BoxShadow(
+          //     color: Colors.black12,
+          //     offset: Offset(0.0, 1.0), //(x,y)
+          //     blurRadius: 10.0,
+          //   ),
+          // ],
+        ),
+        child: cardChild,
+      ),
+    );
+  }
+}
+
+class MonitorCard extends StatelessWidget {
+  const MonitorCard({
+    Key? key,
+    required this.cardIcon,
+    required this.label,
+    required this.currentValue,
+    required this.child,
+  }) : super(key: key);
+
+  final IconData cardIcon;
+  final String label;
+  final String currentValue;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedCard(
+      cardChild: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(cardIcon),
+                SizedBox(
+                  width: 10.w,
+                ),
+                Text(label, style: labelTextStyle),
+              ],
             ),
+            Text(currentValue, style: labelTextStyle),
           ],
         ),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child,
+      ]),
+    );
+  }
+}
+
+class ControlCard extends StatelessWidget {
+  const ControlCard({
+    Key? key,
+    required this.cardIcon,
+    required this.label,
+    required this.currentValue,
+    required this.child,
+    this.onTap,
+  }) : super(key: key);
+
+  final IconData cardIcon;
+  final String label;
+  final String currentValue;
+  final Widget child;
+  final Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => print("tapped"),
+      // borderRadius: circularBorderRadius,
+      child: ElevatedCard(
+        cardChild: Column(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Row(
                 children: [
                   Icon(cardIcon),
-                  const Padding(
-                    padding: EdgeInsets.only(right: 10),
+                  SizedBox(
+                    width: 10.w,
                   ),
-                  Text(label,
-                      style: TextStyle(
-                          fontSize: 14.w, fontWeight: FontWeight.bold)),
+                  Text(label, style: labelTextStyle),
                 ],
               ),
-              Text(currentValue,
-                  style:
-                      TextStyle(fontSize: 14.w, fontWeight: FontWeight.bold)),
+              Text(currentValue, style: labelTextStyle),
             ],
           ),
           child,
