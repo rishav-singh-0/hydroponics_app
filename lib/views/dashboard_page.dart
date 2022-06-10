f75695463ee6baec33d9416d47a15b14055e4d96
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-10 22:53:49 +0530

    removed unused import

--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hydroponics_app/views/components/gauge_card.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
