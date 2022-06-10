815f2a676848f019d59137ddd252dcf802241e16
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-11 03:24:44 +0530

    added basic layout

--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hydroponics_app/views/components/custom_card.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -10,15 +12,46 @@ class DashboardPage extends StatelessWidget {
       body: ListView(physics: const BouncingScrollPhysics(), children: [
         Padding(
           padding: EdgeInsets.only(
-            left: 28.w,
-            right: 18.w,
-            top: 36.h,
+            top: 20.h,
           ),
           child: Column(
             children: [
               Text("Dashboard",
                   style:
                       TextStyle(fontSize: 34.w, fontWeight: FontWeight.bold)),
+              Row(
+                children: const [
+                  ControlCard(
+                      label: "Main Motor",
+                      cardIcon: FontAwesomeIcons.fan,
+                      currentValue: "ON",
+                      child: Text("Some else")),
+                  ControlCard(
+                      label: "Exhost Motor",
+                      cardIcon: FontAwesomeIcons.fan,
+                      currentValue: "ON",
+                      child: Text("Some else"))
+                ],
+              ),
+              const ControlCard(
+                  label: "Light",
+                  cardIcon: FontAwesomeIcons.lightbulb,
+                  currentValue: "ON",
+                  child: Text("Some else")),
+              Row(
+                children: const [
+                  ControlCard(
+                      label: "Acid Motor",
+                      cardIcon: FontAwesomeIcons.fan,
+                      currentValue: "ON",
+                      child: Text("Some else")),
+                  ControlCard(
+                      label: "Base Motor",
+                      cardIcon: FontAwesomeIcons.fan,
+                      currentValue: "ON",
+                      child: Text("Some else")),
+                ],
+              ),
             ],
           ),
         ),
