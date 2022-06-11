b209123701dc2757b51bd7795e29377bd2650878
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-11 13:05:37 +0530

    added models

--- lib/models/monitor.dart
+++ lib/models/monitor.dart
@@ -1,33 +1,52 @@
-class Monitor {
-  final int id;
-  final num temperature;
-  final num humidity;
-  final int moisture;
-  final int light;
-  final num pH;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  Monitor(
-      {required this.id,
-      required this.temperature,
+class MonitorModel {
+  String? documentId;
+  late num temperature;
+  late num humidity;
+  late int moisture;
+  late int light;
+  late num pH;
+
+  MonitorModel(
+      {required this.temperature,
       required this.humidity,
       required this.light,
       required this.moisture,
       required this.pH});
+
+  MonitorModel.fromDocumentSnapshot(
+      {required DocumentSnapshot documentSnapshot}) {
+    documentId = documentSnapshot.id;
+    temperature = documentSnapshot["temperature"];
+    humidity = documentSnapshot["humidity"];
+    light = documentSnapshot["light"];
+    moisture = documentSnapshot["moisture"];
+    pH = documentSnapshot["pH"];
+  }
 }
 
 class Actuator {
-  final int id;
-  final num motorAcid;
-  final num motorBase;
-  final bool motorMain;
-  final bool motorExhaust;
-  final num light;
+  String? documentId;
+  late num motorAcid;
+  late num motorBase;
+  late bool motorMain;
+  late bool motorExhaust;
+  late num light;
 
   Actuator(
-      {required this.id,
-      required this.light,
+      {required this.light,
       required this.motorAcid,
       required this.motorBase,
       required this.motorExhaust,
       required this.motorMain});
+
+  Actuator.fromDocumentSnapshot({required DocumentSnapshot documentSnapshot}) {
+    documentId = documentSnapshot.id;
+    motorAcid = documentSnapshot["motorAcid"];
+    motorBase = documentSnapshot["motorBase"];
+    motorExhaust = documentSnapshot["motorExhaust"];
+    motorMain = documentSnapshot["motorMain"];
+    light = documentSnapshot["light"];
+  }
 }
