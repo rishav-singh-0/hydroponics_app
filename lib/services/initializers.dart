19a068a9460ca366d1c85fddaa815a349b01bfb9
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-11 17:08:57 +0530

    added initializers.dart

---
+++ lib/services/initializers.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+// AuthController authController = AuthController.instance;
+final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
+// FirebaseAuth auth = FirebaseAuth.instance;
+FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
