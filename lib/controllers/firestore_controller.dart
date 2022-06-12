0faa28f4e4c8af6e23c976fcd35ca4e3ab1138b1
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-12 15:58:32 +0530

    controller working

--- lib/controllers/firestore_controller.dart
+++ lib/controllers/firestore_controller.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
-import 'package:hydroponics_app/models/monitor_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../services/firestore_db.dart';
+import 'package:hydroponics_app/models/monitor_model.dart';
+import '../services/initializers.dart';
 
 class MonitorController extends GetxController {
   Rx<List<MonitorModel>> monitorList = Rx<List<MonitorModel>>([]);
@@ -9,5 +13,6 @@ class MonitorController extends GetxController {
   @override
   void onReady() {
     monitorList.bindStream(FirestoreDb.monitorStream());
+    log("onReady method called !!!");
   }
 }
--- lib/services/firestore_db.dart
+++ lib/services/firestore_db.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'initializers.dart';
 import '../models/monitor_model.dart';
@@ -5,16 +7,30 @@ import '../models/monitor_model.dart';
 class FirestoreDb {
   static Stream<List<MonitorModel>> monitorStream() {
     return firebaseFirestore
-        .collection('users')
+        .collection('monitor')
         .snapshots()
         .map((QuerySnapshot query) {
       List<MonitorModel> monitors = [];
       for (var monitor in query.docs) {
         final monitorModel =
-            MonitorModel.fromDocumentSnapshot(documentSnapshot: monitor);
+            MonitorModel.fromMap(data: monitor.data() as Map<String, dynamic>);
         monitors.add(monitorModel);
       }
+      log(monitors.toString());
       return monitors;
     });
   }
 }
+
+// class FirestoreDb {
+//   static Stream<List<MonitorModel>> monitorStream() {
+//     List<MonitorModel> monitors = [];
+//     return firebaseFirestore.collection("monitor").snapshots().map((event) {
+//       for (var document in event.docs) {
+//         final monitorModel = MonitorModel.fromMap(data: document.data());
+//         monitors.add(monitorModel);
+//       }
+//       return monitorModel;
+//     });
+//   }
+// }
