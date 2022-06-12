484b9e03e17b8d227965679261e5c9cdb26a146c
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-12 15:59:04 +0530

    updated model objects

--- lib/models/monitor_model.dart
+++ lib/models/monitor_model.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: unnecessary_null_comparison
+
+import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MonitorModel {
@@ -14,7 +17,74 @@ class MonitorModel {
       required this.humidity,
       required this.light,
       required this.moisture,
-      required this.pH});
+      required this.pH,
+      timestamp});
+
+  factory MonitorModel.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return MonitorModel(
+      temperature: data?['temperature'],
+      humidity: data?['humidity'],
+      moisture: data?['moisture'],
+      light: data?['light'],
+      pH: data?['pH'],
+      timestamp: data?['time_stamp'],
+    );
+  }
+
+  MonitorModel.fromMap({required Map<String, dynamic> data}) {
+    log("testing");
+    log(data.toString());
+    data.entries.forEach((element) {
+      log("${element.key}: ${element.value}");
+      switch (element.key) {
+        case "temperature":
+          {
+            temperature = element.value;
+          }
+          break;
+        case "luminocity":
+          {
+            light = element.value;
+          }
+          break;
+        case "humidity":
+          {
+            humidity = element.value;
+          }
+          break;
+        case "pH":
+          {
+            pH = element.value;
+          }
+          break;
+        case "time_stamp":
+          {
+            timestamp = element.value;
+          }
+          break;
+        case "moisture":
+          {
+            moisture = element.value;
+          }
+          break;
+      }
+    });
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (temperature != null) "temperature": temperature,
+      if (humidity != null) "humidity": humidity,
+      if (moisture != null) "moisture": moisture,
+      if (light != null) "light": light,
+      if (pH != null) "pH": pH,
+      if (timestamp != null) "time_stamp": timestamp,
+    };
+  }
 
   MonitorModel.fromDocumentSnapshot(
       {required DocumentSnapshot documentSnapshot}) {
@@ -24,7 +94,7 @@ class MonitorModel {
     light = documentSnapshot["light"];
     moisture = documentSnapshot["moisture"];
     pH = documentSnapshot["pH"];
-    timestamp = documentSnapshot["timestamp"];
+    timestamp = documentSnapshot["time_stamp"];
   }
 }
 
@@ -45,10 +115,10 @@ class Actuator {
 
   Actuator.fromDocumentSnapshot({required DocumentSnapshot documentSnapshot}) {
     documentId = documentSnapshot.id;
-    motorAcid = documentSnapshot["motorAcid"];
-    motorBase = documentSnapshot["motorBase"];
-    motorExhaust = documentSnapshot["motorExhaust"];
-    motorMain = documentSnapshot["motorMain"];
-    light = documentSnapshot["light"];
+    motorAcid = documentSnapshot["motor_acid"];
+    motorBase = documentSnapshot["motor_base"];
+    motorExhaust = documentSnapshot["motor_ventilation"];
+    motorMain = documentSnapshot["motor_main"];
+    light = documentSnapshot["luminosity"];
   }
 }
