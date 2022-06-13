04b9d77d9685dbe5794fc96b971d85fd66c48202
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-13 19:16:18 +0530

    changed fonts

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:hydroponics_app/controllers/bottom_nav_controller.dart';
 import 'package:hydroponics_app/views/about_page.dart';
@@ -25,15 +25,15 @@ class HomePage extends StatelessWidget {
           currentIndex: controller.tabIndex,
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
+              icon: Icon(FontAwesomeIcons.house),
               label: 'Dashboard',
             ),
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.chart_bar),
+              icon: Icon(FontAwesomeIcons.chartSimple),
               label: 'Chart',
             ),
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.info),
+              icon: Icon(FontAwesomeIcons.info),
               label: 'About',
             ),
           ],
