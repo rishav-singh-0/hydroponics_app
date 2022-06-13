b16ffc5c72b440e9b5e174877f76e9851525ff3d
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-06-13 13:05:22 +0530

    updated dashboard_page

--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:hydroponics_app/controllers/firebase_rtdb_controller.dart';
 import 'package:hydroponics_app/views/components/custom_card.dart';
 
 class DashboardPage extends StatelessWidget {
-  const DashboardPage({Key? key}) : super(key: key);
+  DashboardPage({Key? key}) : super(key: key);
+  final ActuatorController actuatorController = Get.put(ActuatorController());
 
   @override
   Widget build(BuildContext context) {
@@ -14,48 +17,69 @@ class DashboardPage extends StatelessWidget {
           padding: EdgeInsets.only(
             top: 20.h,
           ),
-          child: Column(
-            children: [
-              Text("Dashboard",
-                  style:
-                      TextStyle(fontSize: 34.w, fontWeight: FontWeight.bold)),
-              Row(
-                children: const [
+          child: Obx(
+            () {
+              if (actuatorController.actuatorList.value == null) {
+                return Text("Loading");
+              }
+              return Column(
+                children: [
+                  Text("Dashboard",
+                      style: TextStyle(
+                          fontSize: 34.w, fontWeight: FontWeight.bold)),
+                  Row(
+                    children: [
+                      ControlCard(
+                        label: "Main Motor",
+                        cardIcon: FontAwesomeIcons.fan,
+                        currentValue: checkOnOff(
+                            actuatorController.latestActuator.motorMain),
+                      ),
+                      ControlCard(
+                        label: "Exhaust Motor",
+                        cardIcon: FontAwesomeIcons.fan,
+                        currentValue: checkOnOff(
+                            actuatorController.latestActuator.motorExhaust),
+                      )
+                    ],
+                  ),
                   ControlCard(
-                      label: "Main Motor",
-                      cardIcon: FontAwesomeIcons.fan,
-                      currentValue: "ON",
+                      label: "Light",
+                      cardIcon: FontAwesomeIcons.lightbulb,
+                      currentValue:
+                          checkOnOff(actuatorController.latestActuator.light),
                       child: Text("Some else")),
-                  ControlCard(
-                      label: "Exhast Motor",
-                      cardIcon: FontAwesomeIcons.fan,
-                      currentValue: "ON",
-                      child: Text("Some else"))
+                  Row(
+                    children: [
+                      ControlCard(
+                        label: "Acid Motor",
+                        cardIcon: FontAwesomeIcons.fan,
+                        currentValue: checkOnOff(
+                            actuatorController.latestActuator.motorAcid),
+                      ),
+                      ControlCard(
+                        label: "Base Motor",
+                        cardIcon: FontAwesomeIcons.fan,
+                        currentValue: checkOnOff(
+                            actuatorController.latestActuator.motorBase),
+                      ),
+                    ],
+                  ),
                 ],
-              ),
-              const ControlCard(
-                  label: "Light",
-                  cardIcon: FontAwesomeIcons.lightbulb,
-                  currentValue: "ON",
-                  child: Text("Some else")),
-              Row(
-                children: const [
-                  ControlCard(
-                      label: "Acid Motor",
-                      cardIcon: FontAwesomeIcons.fan,
-                      currentValue: "ON",
-                      child: Text("Some else")),
-                  ControlCard(
-                      label: "Base Motor",
-                      cardIcon: FontAwesomeIcons.fan,
-                      currentValue: "ON",
-                      child: Text("Some else")),
-                ],
-              ),
-            ],
+              );
+            },
           ),
         ),
       ]),
     );
   }
+}
+
+String checkOnOff(var value) {
+  if (value.runtimeType == bool && value) {
+    return "ON";
+  } else if (value.runtimeType == int && value > 0) {
+    return "ON";
+  }
+  return "OFF";
 }
